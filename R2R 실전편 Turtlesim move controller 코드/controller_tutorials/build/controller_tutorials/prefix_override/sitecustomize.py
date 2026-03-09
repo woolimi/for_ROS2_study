aa8e6741bb10ff322c270aef3e97b27cc4f74f01
Woolim Park <woolimi91@naver.com>
2026-03-09 11:50:15 +0900

    delete build log install

--- R2R 실전편 Turtlesim move controller 코드/controller_tutorials/build/controller_tutorials/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/woolim/Documents/for_ROS2_study/R2R 실전편 Turtlesim move controller 코드/controller_tutorials/install/controller_tutorials'
